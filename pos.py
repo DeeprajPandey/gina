13182a42c9094f5c722267a1fec5bd259450b5f6
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-11-08 23:31:04 +0530

    Set up GCNLP POS Tagger
    Set up a service account on the ashoka ID and set up the credentials locally. The syntax analysis works as expected. Next step is to store the response in a JSON file and start working on the algorithms.

--- pos.py
+++ pos.py
@@ -4,3 +4,20 @@
 
 import json
 import googleapiclient.discovery
+
+text = 'The quick brown fox jumps over the lazy dog.'
+
+body = {
+	'document': {
+		'type': 'PLAIN_TEXT',
+		'content': text,
+	},
+	'encoding_type': 'UTF32'
+}
+
+service = googleapiclient.discovery.build('language', 'v1')
+
+request = service.documents().analyzeSyntax(body=body)
+response = request.execute()
+
+print(json.dumps(response, indent=2))
