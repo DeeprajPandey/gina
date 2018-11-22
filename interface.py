0ffbe678c83bf7b944e229d8dfacc31539d2b904
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-11-22 16:38:37 +0530

    The system calls are inside the if condition. Passing user inputs as command line arguments

--- interface.py
+++ interface.py
@@ -1,21 +1,15 @@
 import os
 
 if __name__ == "__main__":
-    usrInput = input("Enter your english string.\n")
-    h_accept = input("\nEnter your hindi string.\n")
-    hTrans = h_accept.split()
-'''
-    with open(“temp.txt”, w) as f:
-        f.write(e_accept)
-        f.write(h_accept)
-
-with open(“temp.txt”, r) as f:
-    str = f.readline()
-    h_output = f.readline(2)
-text = h_output.split()
-'''
-os.system("python pos.py")
-os.system("python Adjectives.py")
-os.system("python Adpositions.py")
-os.system("python Nouns.py")
-os.system("python Verbs.py")
+    enInput = input("Enter your english string.\n")
+    hInput = input("\nEnter your hindi string.\n")
+    pos_cmd = "python3 pos.py %s" % enInput
+    adj_cmd = "python3 Adjectives.py %s" % hInput
+    adp_cmd = "python3 Adpositions.py %s" % hInput
+    noun_cmd = "python3 Nouns.py %s" % hInput
+    verb_cmd = "python3 Verbs.py %s" % hInput
+    os.system(pos_cmd)
+    os.system(adj_cmd)
+    os.system(adp_cmd)
+    os.system(noun_cmd)
+    os.system(verb_cmd)
