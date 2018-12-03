26760f96613dba6341085eb870d7b0cdb36056b6
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-12-04 03:31:18 +0530

    Add flavour to translation.
    The seemingly disconnected, boring sentences have been changed into a short, broken story. Have to figure out a way to handle versions of nouns in Hindi (kela, kele etc)

--- generate_rules.py
+++ generate_rules.py
@@ -6,11 +6,11 @@ def print_SVO_rules(HN_tokens):
     PN = {}
     # [0][0] to flatten the arrays into an integers
     # Subject changes in first 2 sentences
-    sub_pos = np.where(HN_tokens[0] != HN_tokens[1])[0][0]
+    sub_pos = np.where(HN_tokens[0] != HN_tokens[4])[0][0]
     # Verb in second and third
-    verb_pos = np.where(HN_tokens[1] != HN_tokens[2])[0][0]
+    verb_pos = np.where(HN_tokens[0] != HN_tokens[7])[0][0]
     # Object in third and fourth
-    object_pos = np.where(HN_tokens[2] != HN_tokens[3])[0][0]
+    object_pos = np.where(HN_tokens[0] != HN_tokens[5])[0][0]
 
     # using the position in the sentence as a key (string only) and storing the value
     SVO[sub_pos] = 'Subject'
@@ -18,13 +18,13 @@ def print_SVO_rules(HN_tokens):
     SVO[object_pos] = 'Object'
     
     # Get the noun we are using in the next two sentences
-    noun = HN_tokens[3][object_pos]
+    noun = HN_tokens[0][object_pos]
     # The noun must have changed it's position as preposition might come with morphemes
-    new_noun_index = np.where(noun == HN_tokens[4])[0][0]
+    new_noun_index = np.where(noun == HN_tokens[1])[0][0]
     
     # Preposition changes in fifth and sixth
     # Store the index of the pre/post-position
-    prep_pos = np.where(HN_tokens[4] != HN_tokens[5])[0][0]
+    prep_pos = np.where(HN_tokens[1] != HN_tokens[3])[0][0]
     
     if prep_pos < new_noun_index:
         print("Prepositions come before Nouns in your language.")
@@ -39,8 +39,8 @@ def print_SVO_rules(HN_tokens):
 # Subject changes in 0,1
 # Verb changes in 1,2
 # Object changes in 2,3
-EN_Sentences = ["The girl eats the banana.", "The fly eats the banana.", "The fly throws the banana", "The fly throws the fruit.", "The fly is near the fruit.", "The fly is on top of the fruit."]
-HN_Sentences = ["Ladki ne kela khaya.", "Makhi ne kela khaya.", "Makhi ne kela feka.", "Makhi ne fal feka.", "Makhi fal ke paas hai", "Makhi fal ke upar hai."]
+EN_Sentences = ["The girl eats the banana.", "The fly is near the banana.", "The small fly.", "The fly is on top of the banana.", "The fly eats the banana.", "The girl eats the fly.", "The sad fly.", "The girl throws the banana.", "The sad girl."]
+HN_Sentences = ["Ladki ne kela khaya.", "Makhi kela ke paas hai", "Chhoti makhi.", "Makhi kela ke upar hai.", "Makhi ne kela khaya.", "Ladki ne makhi khaya.", "Dukhi makhi.", "Ladki ne kela feka.", "Dukhi ladki."]
 HN_tokens = []
 # Regex to strip input of punctuation
 regex = re.compile('[%s]' % re.escape(string.punctuation))
