7d95854f2729c744275f3b4597e953f4a0c4bab1
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-12-03 23:07:29 +0530

    Moved to rule dictionary
    Extract the position of S, V, & O from the Hindi sentences (relying on the changes introduced in the English sentences) and use stringified versions of those integers as keys in a dictionary. The values that these keys will have will be the corresponding type (Subject, Verb, Object).

--- generate_rules.py
+++ generate_rules.py
@@ -1,12 +1,27 @@
 import re, string
 import numpy as np
-EN_Sentences = ["The girl eats the banana.", "The fly eats the banana."]
-HN_Sentences = ["Ladki ne kela khaya.", "Ladke ne kela khaya."]
+# Subject changes in 0,1
+# Verb changes in 1,2
+# Object changes in 2,3
+EN_Sentences = ["The girl eats the banana.", "The fly eats the banana.", "The fly throws the banana", "The fly throws the fruit."]
+HN_Sentences = ["Ladki ne kela khaya.", "Makhi ne kela khaya.", "Makhi ne kela feka.", "Makhi ne fal feka."]
 HN_tokens = []
+# Regex to strip input of punctuation
 regex = re.compile('[%s]' % re.escape(string.punctuation))
-# We know that the subject changes in the first two sentences.
-# Let's find the changing word in the hindi sentences and store their position.
+# Tokenize the inputs. HN_tokens = [['hello','world']['sentence','two']]
 for h_st in HN_Sentences:
-	tokenize = np.array(regex.sub('', h_st).split())
-	HN_tokens.append(tokenize)
-print(np.where(HN_tokens[0] != HN_tokens[1]))
+    tokenize = np.array(regex.sub('', h_st).split())
+    HN_tokens.append(tokenize)
+#print(np.sum(HN_tokens[0] == HN_tokens[1]))
+# Subject changes in the first two sentences.
+# Find the changing word in the hindi sentences and store their position.
+SVO = {}
+# [0][0] to flatten the arrays into an integers
+sub_pos = np.where(HN_tokens[0] != HN_tokens[1])[0][0]
+verb_pos = np.where(HN_tokens[1] != HN_tokens[2])[0][0]
+object_pos = np.where(HN_tokens[2] != HN_tokens[3])[0][0]
+
+# using the position in the sentence as a key (string only) and storing the value
+SVO[np.array_str(sub_pos)] = 'Subject'
+SVO[np.array_str(verb_pos)] = 'Verb'
+SVO[np.array_str(object_pos)] = 'Object'
