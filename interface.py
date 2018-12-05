5f2b0a68551344e40ac2472bca101d38e1ca011a
Abhinav Masalia <abhinav.masalia99@gmail.com>
2018-12-06 04:28:32 +0530

    Multiple changes including code refinement, adding more options etc.
    Gina now displays a progress bar of how much she learnt from her latest input. More options added to the menu which includes the ability to print Gina's knowledge of nouns and verbs.

--- interface.py
+++ interface.py
@@ -8,11 +8,27 @@ if __name__ == "__main__":
     # in the rest of the python programs
     lex_dict = OrderedDict()
     check = '1'
+    orig_words = 0
+    word_count = 0
+    end_word_count = 0
+    word_list = []
     # We ask the user to input till they wish to stop. Also, after every input, we ask them if they wish to see the lexicon that Gina has developed
     while check == '1':
+        with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+            lex_dict = json.loads(f.read())
+        for category in lex_dict:
+            for key in lex_dict[category]:
+                orig_words = orig_words + 1
+
         enInput = input("Enter your english string.\n")
+        regex = re.compile('[%s]' % re.escape(string.punctuation)) # Using regex to ignore punctuation in the text input
+        word_list = (regex.sub('', enInput)).split() # Store the non-punctuated english sentence's words in a list
+        for elem in word_list:
+            for categ in lex_dict:
+                for indiv in lex_dict[categ]:
+                    if elem == indiv:
+                        word_count = word_count + 1
         h_Input = input("\nEnter your hindi string.\n")
-        regex = re.compile('[%s]' % re.escape(string.punctuation)) # Using regex to ignore punctuation in the hindi text input
         hInput = regex.sub('', h_Input)
         pos_cmd = "python3 pos.py %s" % enInput
         adj_cmd = "python3 Adjectives.py %s" % hInput
@@ -25,40 +41,98 @@ if __name__ == "__main__":
         os.system(adp_cmd)
         os.system(noun_cmd)
         os.system(verb_cmd)
+
         while True: # This loops till the user wants to enter more sentences, on which case it exits the loop and goes back to the start of the outer loop
+            os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
             print("\n=====================================================================================================")
             print("| Enter the corresponding number to choose an option from below.                                    |")
             print("=====================================================================================================")
             print("| 1. Enter more sentences for Gina to learn from.                                                   |")
-            print("| 2. Print all the pronouns that Gina knows.                                                        |")
-            print("| 3. Make Gina take an exam based on whatever has been input and her previously acquired knowledge. |")
-            print("| 4. Exit the program.                                                                              |")
+            print("| 2. Print all the nouns that Gina knows.                                                           |")
+            print("| 3. Print all the verbs that Gina knows.                                                           |")
+            print("| 4. Print all the adjectives that Gina knows.                                                      |")
+            print("| 5. Print all the prepositions that Gina knows.                                                    |")
+            print("| 6. Print all the pronouns that Gina knows.                                                        |")
+            print("| 7. Make Gina take an exam based on whatever has been input and her previously acquired knowledge. |")
+            print("| 8. Exit the program.                                                                              |")
             print("=====================================================================================================\n")
             print("Your choice:", end = "")
             check = input(" ")
             if check == '1':
                 break
             elif check == '2':
-                print("\n---------------------------------")
-                print("|  Pronoun\tHindi Key\t|")
-                print("---------------------------------")
                 with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
-                    lex_dict = json.loads(f.read())
-                    for pronoun in lex_dict["pronouns"]:
-                        if isinstance(lex_dict["pronouns"][pronoun], list):
-                            for i in range(len(lex_dict["pronouns"][pronoun])):
-                                print("|  " + pronoun + "\t\t" + lex_dict["pronouns"][pronoun][i] + "\t\t|")
-                        else:
-                            print("|  " + pronoun + "\t\t" + lex_dict["pronouns"][pronoun] + "\t\t|")
-                print("---------------------------------\n")
+                    temp_dict = json.loads(f.read())
+
+                print("\n-----------------------------")
+                print("|  {:<12} {:<10}  |".format('Noun','Hindi Key'))
+                print("-----------------------------")
+                for noun in temp_dict["nouns"]:
+                    print("|  {:<12} {:<10}  |".format(noun, temp_dict["nouns"][noun][0]))
+                print("-----------------------------\n")
+
                 r = input("Press enter to go back to the menu.")
                 if r == "":
-                    os.system("cls") # Clears the screen
+                    os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
             elif check == '3':
-                os.system(exam_cmd)
+                with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                    temp_dict = json.loads(f.read())
+
+                print("\n--------------------------------------------------------")
+                print("|  {:<12} {:<18} {:<18}  |".format('Noun','Male Inflection','Female Inflection'))
+                print("--------------------------------------------------------")
+                for verb in temp_dict["verbs"]:
+                    print("|  {:<12} {:<18} {:<18}  |".format(verb, temp_dict["verbs"][verb][0], temp_dict["verbs"][verb][1]))
+                print("--------------------------------------------------------\n")
+
                 r = input("Press enter to go back to the menu.")
                 if r == "":
-                    os.system("cls") # Clears the screen
-            elif check == '4':
-                os.system("cls") # Clears the screen
+                    os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
+            elif check == '6':
+                os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
+                with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                    temp_dict = json.loads(f.read())
+
+                print("\n-----------------------------")
+                print("|  {:<12} {:<10}  |".format('Pronoun','Hindi Key'))
+                print("-----------------------------")
+                for pronoun in temp_dict["pronouns"]:
+                    if isinstance(temp_dict["pronouns"][pronoun], list):
+                        for i in range(len(temp_dict["pronouns"][pronoun])):
+                            print("|  {:<12} {:<10}  |".format(pronoun, temp_dict["pronouns"][noun][0]))
+                    else:
+                        print("|  {:<12} {:<10}  |".format(pronoun, temp_dict["pronouns"][pronoun]))
+                print("-----------------------------\n")
+
+                r = input("Press enter to go back to the menu.")
+                if r == "":
+                    os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
+            elif check == '7':
+                os.system(exam_cmd)
+                with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                    temp_dict = json.loads(f.read())
+                for category in temp_dict:
+                    for key in temp_dict[category]:
+                        end_word_count = end_word_count + 1
+                words_learnt = end_word_count - orig_words
+
+                # Prints the progress bar
+                count = (float)(words_learnt/word_count)
+                total = 100
+                suffix = 'learned.'
+                bar_len = 50
+                filled_len = int(round(bar_len * count / float(total)))
+
+                percents = round(100.0 * count / float(total), 1)
+                bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+                sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
+                sys.stdout.flush()
+
+                print("\nPress enter to go back to the menu.")
+                r = input("")
+                if r == "":
+                    os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
+            elif check == '8':
+                os.system("cls||clear") # Clears the screen cls is for windows and clear for Mac and Linux
                 sys.exit()
