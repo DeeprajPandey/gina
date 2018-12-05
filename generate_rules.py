ba6d0cea0e246a335606182e1eb8215ae23127ef
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-12-06 02:05:17 +0530

    Handled multiple code-breaks when testing different languages.
    The object recognition for Spanish was breaking because of the way 'the' inflects based on the gender of the object. The code now accounts for that by ignoring two-letter words that change when checking for objects. Accordingly, the noun that was taken from the object also accommodates for this fact and takes the appropriate word for comparison before looking for the preposition. Fixed a huge bug (actually small) in the way adjective-noun positioning was decided after faulty outputs for Bengali and Odia. Have to make language arrays from beta-participant inputs.

--- generate_rules.py
+++ generate_rules.py
@@ -6,20 +6,20 @@ def hamming_distance(s1, s2):
     # Ensure length of s1 >= s2
     if len(s2) > len(s1):
         s1, s2 = s2, s1
-
+    
     # Distance is difference in length + differing chars
     distance = len(s1) - len(s2)
     for i, c in enumerate(s2):
         if c != s1[i]:
             distance += 1
 
-    return distance
+return distance
 
 # sherlock and watson are the 2 strings we will be dealing with today
 def jaro_winkler_distance(sherlock, watson):
     sherlock_len = len(sherlock)
     watson_len = len(watson)
-
+    
     if not sherlock_len or not watson_len:
         return 0.0
     
@@ -30,15 +30,15 @@ def jaro_winkler_distance(sherlock, watson):
     # Negative search_range was a bug for hours. Ugh.
     if search_range < 0:
         search_range = 0
-    
+
     # Array of flags corresponding to each character, which will toggle to True
     # when they match b/w the strings
     sherlock_flags = [False]*sherlock_len
     watson_flags = [False]*watson_len
 
 
-    # While searching only within the search_range, count & flag matched pairs
-    common_chars = 0
+# While searching only within the search_range, count & flag matched pairs
+common_chars = 0
     for i, sherlock_ch in enumerate(sherlock):
         low = i - search_range if i > search_range else 0
         hi = i + search_range if i + search_range < watson_len else watson_len - 1
@@ -51,10 +51,10 @@ def jaro_winkler_distance(sherlock, watson):
                 # compare the next character in sherlock with the range in watson
                 break
 
-    # If no characters match, m=0
-    if not common_chars:
-        return 0.0
-
+# If no characters match, m=0
+if not common_chars:
+    return 0.0
+    
     # Count transpositions
     # Note: only check order of matched characters.
     # For instance, DwAyNE and DuANE have 0 transpositions since the
@@ -72,25 +72,25 @@ def jaro_winkler_distance(sherlock, watson):
     # We counted once for each character in sherlock.
     # If transpositions exist, they're counted twice
     trans_count /= 2
-
+    
     # Adjust for similarities in nonmatched characters
     common_chars = float(common_chars)
     # Jaro Distance
     weight = ((common_chars/sherlock_len + common_chars/watson_len +
-              (common_chars-trans_count) / common_chars)) / 3
+               (common_chars-trans_count) / common_chars)) / 3
+        
+               # Winkler modification: continue to boost if strings are similar
+               if weight > 0.7 and sherlock_len > 3 and watson_len > 3:
+                   # adjust for up to first 4 chars in common
+                   j = min(min_len, 4)
+                   i = 0
+                       while i < j and sherlock[i] == watson[i] and sherlock[i]:
+                           i += 1
+                               if i:
+                                   # The scaling factor, p, is usually 0.1
+                                   weight += i * 0.15 * (1.0 - weight)
 
-    # Winkler modification: continue to boost if strings are similar
-    if weight > 0.7 and sherlock_len > 3 and watson_len > 3:
-        # adjust for up to first 4 chars in common
-        j = min(min_len, 4)
-        i = 0
-        while i < j and sherlock[i] == watson[i] and sherlock[i]:
-            i += 1
-        if i:
-            # The scaling factor, p, is usually 0.1
-            weight += i * 0.15 * (1.0 - weight)
-
-    return weight
+return weight
 
 
 def print_syntax_rules(HN_tokens):
@@ -101,8 +101,17 @@ def print_syntax_rules(HN_tokens):
     # Verb in second and third
     verb_pos = np.where(HN_tokens[0] != HN_tokens[7])[0][0]
     # Object in third and fourth
+    # object_pos = np.where(HN_tokens[0] != HN_tokens[5])[0][0]
     object_pos = np.where(HN_tokens[0] != HN_tokens[5])[0][0]
-
+    arr1 = []
+    arr2 = []
+    flag = False
+    if len(HN_tokens[0][object_pos]) <= 2:
+        flag = True
+        arr1 = np.delete(HN_tokens[0], object_pos)
+        arr2 = np.delete(HN_tokens[5], object_pos)
+        object_pos = np.where(arr1 != arr2)[0][0]
+    
     # using the position in the sentence as a key (string only) and storing the value
     SVO[sub_pos] = 'Subject'
     SVO[verb_pos] = 'Verb'
@@ -119,30 +128,33 @@ def print_syntax_rules(HN_tokens):
     
     # Get the noun that is constant in the preposition case
     # from when we parsed object
-    noun_p = HN_tokens[0][object_pos].lower()
+    if flag:
+        noun_p = arr1[object_pos].lower()
+    else:
+        noun_p = HN_tokens[0][object_pos].lower()
     
     for word in HN_tokens[1]:
-        if jaro_winkler_distance(noun_p, word) >= 0.9 and hamming_distance(noun_p, word) <= 1:
+        if jaro_winkler_distance(noun_p, word.lower()) >= 0.9 and hamming_distance(noun_p, word.lower()) <= 1:
             # The noun must have changed it's position as preposition might come with morphemes
             new_noun_index_p = np.where(word == HN_tokens[1])[0][0]
 
-    mid1 = len(HN_tokens[1])/2
-    mid2 = len(HN_tokens[3])/2
-    if (new_noun_index_p < mid1 and new_noun_index_p < mid2):
-        print("\nprepositions come after Nouns (postpositions),")
+mid1 = len(HN_tokens[1])/2
+mid2 = len(HN_tokens[3])/2
+if (new_noun_index_p < mid1 and new_noun_index_p < mid2):
+    print("\nprepositions come after Nouns (postpositions),")
     elif (new_noun_index_p >= mid1 and new_noun_index_p >= mid2):
         print("\nprepositions come before Nouns,")
-    else:
-        print("PN/NP couldn't be determined with the data currently available.")
-
-
+else:
+    print("PN/NP couldn't be determined with the data currently available.")
+    
+    
     # if prep_pos < new_noun_index_p:
     #     print("\nprepositions come before Nouns,")
     # elif prep_pos > new_noun_index_p:
     #     print("\nprepositions come after Nouns (postpositions),")
     # else:
     #     print("PN/NP couldn't be determined with the data currently available.")
-        
+    
     
     # Adpositions change in third and seventh
     # ad_pos = np.where(HN_tokens[2] != HN_tokens[6])[0][0]
@@ -151,25 +163,25 @@ def print_syntax_rules(HN_tokens):
     noun_ad = HN_tokens[4][sub_pos].lower()
     
     for word in HN_tokens[2]:
-        if jaro_winkler_distance(noun_ad, word) >= 0.9 and hamming_distance(noun_ad, word) <= 1:
+        if jaro_winkler_distance(noun_ad, word.lower()) >= 0.9 and hamming_distance(noun_ad, word.lower()) <= 1:
             # The noun must have changed it's position as preposition might come with morphemes
             new_noun_index_a = np.where(word == HN_tokens[2])[0][0]
     
     mid1 = len(HN_tokens[2])/2
     mid2 = len(HN_tokens[6])/2
-    if (new_noun_index_a < mid1 and new_noun_index_a < mid2):
+    if (new_noun_index_a <= mid1 and new_noun_index_a <= mid2):
         print("and adjectives come before nouns.")
-    elif (new_noun_index_a >= mid1 and new_noun_index_a >= mid2):
+    elif (new_noun_index_a > mid1 and new_noun_index_a > mid2):
         print("and adjectives come after nouns.")
     else:
         print("AN/NA couldn't be determined with the data currently available.")
 
-    # if ad_pos < new_noun_index_a:
-    #     print("and adjectives come before nouns.")
-    # elif prep_pos > new_noun_index_a:
-    #     print("and adjectives come after nouns.")
-    # else:
-    #     print("AN/NA couldn't be determined with the data currently available.")
+# if ad_pos < new_noun_index_a:
+#     print("and adjectives come before nouns.")
+# elif prep_pos > new_noun_index_a:
+#     print("and adjectives come after nouns.")
+# else:
+#     print("AN/NA couldn't be determined with the data currently available.")
 
 # Subject changes in 0,4 (use noun from 4 for AN)
 # Verb changes in 0,7
