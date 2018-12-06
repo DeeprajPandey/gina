1d60fa7ed81b1c2988f62764d30c560aa9c8a1e4
Abhinav Masalia <abhinav.masalia99@gmail.com>
2018-12-06 12:54:45 +0530

    Refined the menu
    Corrected some printing glitches and previously unnoticed errors. Also, corrected the infinite looping that occured on accessing the input option from the menu.

--- interface.py
+++ interface.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     word_count = 0
     end_word_count = 0
     word_list = []
+    exam_cmd = "python3 exam.py" # Moving it outside the loop, so that the user can directly access the exam without input
     while True: # This loops till the user wants to exit the program
         os.system("clear") # Clears the screen
         print("\n=====================================================================================================")
@@ -25,38 +26,67 @@ if __name__ == "__main__":
         print("=====================================================================================================\n")
         print("Your choice:", end = "")
         check = input(" ")
+        os.system("clear") # Clears the screen
         if check == '1':
             # We don't need to ignore punctuations in the english input here, because when the API is called, the PUNCT tag is specifically skipped
             # in the rest of the python programs
             # We ask the user to input till they wish to stop. Also, after every input, we ask them if they wish to see the lexicon that Gina has developed
-            while check == '1':
-                with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
-                    lex_dict = json.loads(f.read())
-                for category in lex_dict:
-                    for key in lex_dict[category]:
-                        orig_words = orig_words + 1
+            with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                lex_dict = json.loads(f.read())
+            for category in lex_dict:
+                for key in lex_dict[category]:
+                    orig_words = orig_words + 1
 
-                enInput = input("Give Gina an English sentence:\n")
-                regex = re.compile('[%s]' % re.escape(string.punctuation)) # Using regex to ignore punctuation in the text input
-                word_list = (regex.sub('', enInput)).split() # Store the non-punctuated english sentence's words in a list
-                for elem in word_list:
-                    for categ in lex_dict:
-                        for indiv in lex_dict[categ]:
-                            if elem == indiv:
-                                word_count = word_count + 1
-                h_Input = input("\nGive Gina its Hindi translation:\n")
-                hInput = regex.sub('', h_Input)
-                pos_cmd = "python3 pos.py %s" % enInput
-                adj_cmd = "python3 Adjectives.py %s" % hInput
-                adp_cmd = "python3 Adpositions.py %s" % hInput
-                noun_cmd = "python3 Nouns.py %s" % hInput
-                verb_cmd = "python3 Verbs.py %s" % hInput
-                exam_cmd = "python3 exam.py"
-                os.system(pos_cmd)
-                os.system(adj_cmd)
-                os.system(adp_cmd)
-                os.system(noun_cmd)
-                os.system(verb_cmd)
+            print("Give Gina an English sentence:\n")
+            enInput = input("")
+            regex = re.compile('[%s]' % re.escape(string.punctuation)) # Using regex to ignore punctuation in the text input
+            word_list = (regex.sub('', enInput)).split() # Store the non-punctuated english sentence's words in a list
+            for elem in word_list:
+                for categ in lex_dict:
+                    for indiv in lex_dict[categ]:
+                        if elem == indiv:
+                            word_count = word_count + 1
+            print("\nGive Gina its Hindi translation:\n")
+            h_Input = input("")
+            hInput = regex.sub('', h_Input)
+            pos_cmd = "python3 pos.py %s" % enInput
+            adj_cmd = "python3 Adjectives.py %s" % hInput
+            adp_cmd = "python3 Adpositions.py %s" % hInput
+            noun_cmd = "python3 Nouns.py %s" % hInput
+            verb_cmd = "python3 Verbs.py %s" % hInput
+            os.system(pos_cmd)
+            os.system(adj_cmd)
+            os.system(adp_cmd)
+            os.system(noun_cmd)
+            os.system(verb_cmd)
+            os.system(exam_cmd)
+
+            with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                temp_dict = json.loads(f.read())
+            for category in temp_dict:
+                for key in temp_dict[category]:
+                    end_word_count = end_word_count + 1
+            words_learnt = end_word_count - orig_words
+
+            print("\n")
+            # Prints the progress bar
+            count = float(words_learnt/word_count)*100
+            total = 100
+            suffix = 'learned.'
+            bar_len = 50
+            filled_len = int(round(bar_len * count / float(total)))
+
+            percents = round(100.0 * count / float(total), 1)
+            bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+            sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
+            sys.stdout.flush()
+
+            print("\n")
+            print("Press enter to go back to the menu.", end = "")
+            r = input("\n")
+            if r == "":
+                os.system("clear") # Clears the screen
         elif check == '2':
             with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
                 temp_dict = json.loads(f.read())
@@ -76,7 +106,7 @@ if __name__ == "__main__":
                 temp_dict = json.loads(f.read())
 
             print("\n--------------------------------------------------------")
-            print("|  {:<12} {:<18} {:<18}  |".format('Noun','Male Inflection','Female Inflection'))
+            print("|  {:<12} {:<18} {:<18}  |".format('Verb','Male Inflection','Female Inflection'))
             print("--------------------------------------------------------")
             for verb in temp_dict["verbs"]:
                 print("|  {:<12} {:<18} {:<18}  |".format(verb, temp_dict["verbs"][verb][0], temp_dict["verbs"][verb][1]))
@@ -85,8 +115,35 @@ if __name__ == "__main__":
             r = input("Press enter to go back to the menu.")
             if r == "":
                 os.system("clear") # Clears the screen
+        elif check == '4':
+            with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                temp_dict = json.loads(f.read())
+
+            print("\n--------------------------------------------------------")
+            print("|  {:<12} {:<18} {:<18}  |".format('Adjective','Male Inflection','Female Inflection'))
+            print("--------------------------------------------------------")
+            for adj in temp_dict["adjectives"]:
+                print("|  {:<12} {:<18} {:<18}  |".format(adj, temp_dict["adjectives"][adj][0], temp_dict["adjectives"][adj][1]))
+            print("--------------------------------------------------------\n")
+
+            r = input("Press enter to go back to the menu.")
+            if r == "":
+                os.system("clear") # Clears the screen
+        elif check == '5':
+            with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
+                temp_dict = json.loads(f.read())
+
+            print("\n-----------------------------")
+            print("|  {:<12} {:<10}  |".format('Preposition','Postposition'))
+            print("-----------------------------")
+            for prep in temp_dict["adpositions"]:
+                print("|  {:<12} {:<10}  |".format(prep, temp_dict["adpositions"][prep]))
+            print("-----------------------------\n")
+
+            r = input("Press enter to go back to the menu.")
+            if r == "":
+                os.system("clear") # Clears the screen
         elif check == '6':
-            os.system("clear") # Clears the screen
             with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
                 temp_dict = json.loads(f.read())
 
@@ -96,7 +153,7 @@ if __name__ == "__main__":
             for pronoun in temp_dict["pronouns"]:
                 if isinstance(temp_dict["pronouns"][pronoun], list):
                     for i in range(len(temp_dict["pronouns"][pronoun])):
-                        print("|  {:<12} {:<10}  |".format(pronoun, temp_dict["pronouns"][noun][0]))
+                        print("|  {:<12} {:<10}  |".format(pronoun, temp_dict["pronouns"][pronoun][0]))
                 else:
                     print("|  {:<12} {:<10}  |".format(pronoun, temp_dict["pronouns"][pronoun]))
             print("-----------------------------\n")
@@ -106,30 +163,9 @@ if __name__ == "__main__":
                 os.system("clear") # Clears the screen
         elif check == '7':
             os.system(exam_cmd)
-            with open('JSON/lexicon.json', 'r') as f: #calling lexicon.json for read and storing it into a dictionary
-                temp_dict = json.loads(f.read())
-            for category in temp_dict:
-                for key in temp_dict[category]:
-                    end_word_count = end_word_count + 1
-            words_learnt = end_word_count - orig_words
-
-            # Prints the progress bar
-            count = (float)(words_learnt/word_count)
-            total = 100
-            suffix = 'learned.'
-            bar_len = 50
-            filled_len = int(round(bar_len * count / float(total)))
-
-            percents = round(100.0 * count / float(total), 1)
-            bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-            sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
-            sys.stdout.flush()
-
             print("\nPress enter to go back to the menu.")
             r = input("")
             if r == "":
                 os.system("clear") # Clears the screen
         elif check == '8':
-            os.system("clear") # Clears the screen
             sys.exit()
